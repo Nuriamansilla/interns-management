f35cb8df74c3411ea114225370909368482d5a61
tmmagnaldi <tm.magnaldi@gmail.com>
2026-01-13 15:36:40 +0100

    basic sample data

--- src/main/java/com/example/entities/Intern.java
+++ src/main/java/com/example/entities/Intern.java
@@ -5,18 +5,12 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -46,21 +40,20 @@ public class Intern implements Serializable {
 
     private Long globalID;
 
-
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @Enumerated(EnumType.STRING)
     private Center center;
 
-   // @OneToOne
+    // @OneToOne
     private AcademicInformation academicInformation;
     
-  //  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "intern")
-   // @JsonIgnore
+    // @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "intern")
+    // @JsonIgnore
     private Language language;
 
-  //  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "intern")
-  //  @JsonIgnore
+    // @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "intern")
+    // @JsonIgnore
     private HRfeedback hrfeedback;
 }
