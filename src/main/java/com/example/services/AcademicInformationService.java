f9be189e3fb95fb8b56ef4cf97bd47b53d1435d8
amelbelam <amelbelam@hotmail.com>
2026-01-14 17:41:55 +0100

    services de academic inf , languages y feedback

---
+++ src/main/java/com/example/services/AcademicInformationService.java
@@ -0,0 +1,15 @@
+package com.example.services;
+
+import java.util.List;
+
+import com.example.entities.AcademicInformation;
+
+public interface AcademicInformationService {
+
+    List<AcademicInformation> findAllAcademicInformations();
+    AcademicInformation findAcademicInformationById(int id);
+    AcademicInformation saveAcademicInformation(AcademicInformation academicInformation);
+   
+
+
+}
