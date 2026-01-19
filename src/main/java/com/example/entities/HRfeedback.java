89e98ac24e67f2f5fd4c03f581daf6df59006116
tmmagnaldi <tm.magnaldi@gmail.com>
2026-01-19 17:13:39 +0100

    comprobaciones y correcciones

--- src/main/java/com/example/entities/HRfeedback.java
+++ src/main/java/com/example/entities/HRfeedback.java
@@ -2,10 +2,14 @@ package com.example.entities;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +31,9 @@ public class HRfeedback implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-
+    // CORRECTO: NO MODIFICAR   
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Intern intern;
     
 }
