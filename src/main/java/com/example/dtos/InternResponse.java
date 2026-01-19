e58de4914cf2d84335ad5e4857bfedc7158e2d3d
dhe883 <dhekrabeloumi1@gmail.com>
2026-01-19 15:59:53 +0100

    parte de dto y record done

---
+++ src/main/java/com/example/dtos/InternResponse.java
@@ -0,0 +1,20 @@
+package com.example.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record InternResponse(
+
+     int id,
+     String name,
+     String surname1,
+     String surname2,
+     long globalID,
+     String gender,
+     String center, 
+
+     // campos para mapear la entidad academic information , a continuacion 
+
+     String title
+
+) {}
