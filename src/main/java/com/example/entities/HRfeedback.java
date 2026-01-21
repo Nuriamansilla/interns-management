c5881d6446f18f1f6e629095d6ff7747930c6969
dhe883 <dhekrabeloumi1@gmail.com>
2026-01-21 16:40:33 +0100

    merge parte de dhekra terminado y funciona

--- src/main/java/com/example/entities/HRfeedback.java
+++ src/main/java/com/example/entities/HRfeedback.java
@@ -7,7 +7,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -51,11 +51,6 @@ public class HRfeedback implements Serializable{
     @NotNull(message = "At least one feedback is required is requiered")
     @Size(max = 1000, message = "feedbacks must not exceed 1000 characters")
     private String comments;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JsonIgnore
-    private Intern intern; 
-
 
     // CORRECTO: NO MODIFICAR   
     @ManyToOne(fetch = FetchType.LAZY)
