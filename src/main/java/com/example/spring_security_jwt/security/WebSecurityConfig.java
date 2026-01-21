8555429fe47ee478a21c506d3d8500ddbab036a0
tmmagnaldi <tm.magnaldi@gmail.com>
2026-01-21 17:58:46 +0100

    security - manejo de peticiones

---
+++ src/main/java/com/example/spring_security_jwt/security/WebSecurityConfig.java
@@ -0,0 +1,90 @@
+package com.example.spring_security_jwt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.spring_security_jwt.security.jwt.AuthEntryPointJwt;
+import com.example.spring_security_jwt.security.jwt.AuthTokenFilter;
+import com.example.spring_security_jwt.security.jwt.JwtUtils;
+import com.example.spring_security_jwt.security.services.UserDetailsServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableMethodSecurity
+/**
+ * La anotacion anterior permite securedEnable = true, jsr250Enabled = true y la mas importante
+ * prePostEnabled = true.
+ * 
+ * Que se resume a poder asegurar directamente los metodos de los controladores, es decir,
+ * donde se delegan las peticiones, concretamente los endpoints
+ */
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthEntryPointJwt unauthorizeHandle;
+    private final JwtUtils jwtUtils;
+
+    @Bean
+    AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter(jwtUtils, userDetailsService);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws  Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    // ESTE ES EL MÉTODO QUE NECESITARÁ ADAPTACIÓN PARA EL PROYECTO FINAL
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            // Para saber si la petición está autenticada o no
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizeHandle))
+            // Para verificar si la API es sin estado, que es lo que nos interesa por lo general
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            /* Aquí es donde entran nuestros cambios:
+                   Estoy diciendo que las peticiones que lleguen con HTTP tienen que estar
+                   autorizadas, y solo voy a permitir las que vengan por esta ruta.
+                   Para ello crearemos un controller para hacer signup y signin, para que si
+                   entro por ahí no me pida ningún token ni nada. En este path permito hacer todo.
+                   Cualquier otro endpoint o url tiene que estar autenticado para poder acceder. */
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated());
+
+        http.authenticationProvider(authenticationProvider());
+        
+        http.addFilterBefore(authenticationJwtTokenFilter(), 
+            UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
